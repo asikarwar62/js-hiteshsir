cdbc2e3cbc605206ab19dca7404d688b06fc807f
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-10 13:04:56 +0000

    variables

---
+++ 01_basic/02_variables.js
@@ -0,0 +1,18 @@
+var name = "anjali";
+const surName = "sikawar";
+let fatherName = "dharmendrasingh";
+motherName = "meenben";
+let brotherName;
+
+name = "naina";
+// surName = "suman";
+fatherName = "jitendra";
+motherName = " minaben";
+
+
+
+
+console.log(name);
+console.table([name, surName, fatherName ,motherName, brotherName]);
+
+//
