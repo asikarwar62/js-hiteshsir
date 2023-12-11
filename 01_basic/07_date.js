232c48a7768cff66041508cd9565bc68793e66a0
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-11 20:02:42 +0000

    date.js

---
+++ 01_basic/07_date.js
@@ -0,0 +1,15 @@
+const myDate = new Date();
+// myDate.setFullYear("2024-04-14");
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toUTCString());
+console.log(myDate.getMilliseconds());
+console.log(Math.floor(Date.now()/1000));
+console.log(myDate.getMonth());
+console.log(myDate.getUTCDate());
+console.log(myDate.getUTCMinutes());
+console.log(myDate.getHours());
+console.log(myDate.setFullYear(2024));
+console.log(myDate.getFullYear());
