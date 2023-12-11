3ac69fc763776c5b1b4ca9898c017c49730d3426
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-11 19:23:02 +0000

    numberMath.js

---
+++ 01_basic/06_numberMath.js
@@ -0,0 +1,24 @@
+// const score = 91;
+// const gautam = new Number(97678);
+
+// console.log(score);
+// console.log(gautam);
+
+// console.log(gautam.toString().length);
+// console.log(gautam.toFixed(4));
+// console.log(gautam.toPrecision(2));
+// console.log(gautam.toLocaleString('en-IN'));
+
+const score = -2.7;
+const score1 = 30;
+const score2 = 6.7;
+const score3 = 7.8;
+console.log(Math.max(score2,score3));
+
+const score4 = Math.floor((Math.random()) * 10 )+1;
+console.log(score4);
+
+const min = 20;
+const max = 100;
+const range = Math.floor((Math.random()) * ((max -min) + 1)) + min;
+console.log(range);
