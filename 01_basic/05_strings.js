149073f2303d80359573df3a65bf3e99c5baa4e0
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-11 15:58:21 +0000

    string

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,9 @@
+console.log("hello " + "world");
+let name = "anjali";
+let surName = "sikarwar";
+console.log("My fullname is "+name +" "+surName);
+
+let newName = new String("Anjali");
+console.log(newName);
+console.log(newName.sup());
+console.log(newName.__proto__);
