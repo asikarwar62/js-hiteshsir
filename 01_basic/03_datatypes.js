57c13d0fbfe39476b42bc378fd36e6684583bf25
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-11 10:14:56 +0000

    datatypes

---
+++ 01_basic/03_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let a = 2;
+let name = "anjali";
+let gender = true;
+
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.table([a,name,gender]);
---
+++ 01_basic/04_conversion.js
@@ -0,0 +1,9 @@
+let name = "";
+
+console.log(name);
+console.log(typeof name);
+
+let newName = Boolean(name);
+console.log(newName);
+console.log(typeof newName);
+
