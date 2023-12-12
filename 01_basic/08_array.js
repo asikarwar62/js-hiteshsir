47ea65ac81a264e7cb8b92aa4625063e65841541
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-12 11:42:50 +0000

    array

---
+++ 01_basic/08_array.js
@@ -0,0 +1,35 @@
+// const arr1 = [1,2,3,4,5,6];
+// const arr2 = ["anjali"];
+// console.log(arr1[0]);
+
+// console.log(arr1.push(7));
+// console.log(arr1.pop());
+// console.log(arr1.shift());
+// console.log(arr1.includes(2));
+// console.log(arr1.indexOf(2));
+// console.log(arr1.join(arr2));
+// console.log(arr1.splice(1,3));
+// console.log(arr1);
+// console.log(arr1.slice(1,3));
+// console.log(arr1);
+
+let arr3 = [1,2,3,4,5];
+let arr4 = [6,7,8,9];
+
+// console.log(arr3.splice(2,2,1));
+// console.log(arr3);
+
+// console.log(arr3.slice(1,3));
+// console.log(arr3);
+
+// console.log(arr3.push(arr4));
+// console.log(arr3);
+// // console.log(arr3.concat(arr4));
+// const newArr = [...arr3 ,...arr4];
+// console.log(newArr);
+
+// const newArr = [1,2,3,4,[5,6,[7,8],9,10,11,12,13],14];
+// console.log(newArr.flat(Infinity));
+// console.log(Array.isArray("hitesh"));
+
+console.log(Array.of("Hitesh"));
