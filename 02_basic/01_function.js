61d399b3fec3495e3cb4f99bc94b686e0103ebff
asikarwar62 <129068653+asikarwar62@users.noreply.github.com>
2023-12-12 19:03:50 +0000

    function

---
+++ 02_basic/01_function.js
@@ -0,0 +1,42 @@
+// function sum(num1=1,num2=1) {
+//     return (num1+num2);
+// }
+// const result = sum();
+// // console.log(result);
+
+
+// function enterUsername(username) {
+//     if(!username){
+//         console.log(`this is ${username}`);
+//     }
+//     else{
+//     console.log(`this is ${username}`);
+// }
+// }
+
+// enterUsername("Anjali");
+// enterUsername();
+
+// function shoppingCart(num1,num2,...nums) {
+//     console.log(num1,num2,nums);
+// }
+
+// console.log(shoppingCart(200,300,400,500));
+
+// const introduction = {
+//     name: "Anjali",
+//     surName: "Sikarwar"
+// }
+
+// function intro(myObj) {
+//     console.log(`My name is ${myObj.name}  ${myObj.surName}`);
+// }
+
+// intro(introduction);
+
+const array = [2,3,4,5,6];
+function arry(ary1) {
+    console.log(ary1);
+}
+
+arry(array);
